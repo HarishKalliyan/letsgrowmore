52e68229798ce71a3cb2619f102b746eb8d5139d
HarishKalliyan <harishkalliyan@gmail.com>
2023-10-28 21:10:45 +0530

    New Files task 4

--- Image to Pencil sketch .py
+++ Image to Pencil sketch .py
@@ -1,30 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Image to Pencil sketch
-# 
-
-# ## Procedure
-1.Import pages
-2.Accept the Image
-3.Convert into Gray Scale
-4.Convert into Inverse GrayScale
-5.Display the Pencil and Normal Image
-# In[1]:
-
-
 import cv2
-
-
-# In[2]:
-
 
 image=cv2.imread("dog.jpg")
 cv2.imshow("Normal Image",image)
 cv2.waitKey(0)
-
-
-# In[3]:
 
 
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
@@ -32,23 +10,12 @@ cv2.imshow("Gray Scale Image",gray)
 cv2.waitKey(0)
 
 
-# In[4]:
-
-
 invers=255-gray
 cv2.imshow("Inverse GrayScale Image",invers)
 cv2.waitKey(0)
 
-
-# In[5]:
-
-
 blur=cv2.GaussianBlur(invers,(21,21),0)
 inver_blur=255-blur
-
-
-# In[6]:
-
 
 pencil=cv2.divide(gray,inver_blur,scale=256.0)
 cv2.imshow("Normal Image",image)
